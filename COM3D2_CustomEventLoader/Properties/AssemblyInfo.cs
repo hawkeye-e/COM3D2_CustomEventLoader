f05e79988d087046dc7ea3b64c6ad77e4ea02922
hawkeye-e <heroselis@gmail.com>
2025-06-05 15:24:34 +0100

    Update assembly info

--- COM3D2_CustomEventLoader/Properties/AssemblyInfo.cs
+++ COM3D2_CustomEventLoader/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("COM3D2_CustomEvent")]
+[assembly: AssemblyTitle("COM3D2_CustomEventLoader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("COM3D2_CustomEvent")]
-[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("COM3D2_CustomEventLoader")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
